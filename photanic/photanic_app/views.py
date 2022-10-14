e8d0b41c06e7e4b0e89e8b38af5652820c82d8fb
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-10-14 16:18:15 +0200

    Update views.py

--- photanic/photanic_app/views.py
+++ photanic/photanic_app/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from photanic_app.models import Usuario, Planta, Foto, Comentario, Articulo, Valoracion
+from django.views.generic import DetailView
+from django.views.generic.edit import FormView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
+
+def index (request):
+    return render(request, 'photanic_app/index.html')
+
+class Lista_plantas(ListView):
+    model = Planta
+
+class Detalles_plantas(DetailView):
+    model = Planta
+
+class Lista_fotos(ListView):
+    model = Foto
+
+class Detalles_fotos(DetailView):
+    model = Foto
+
+class Lista_comentarios(ListView):
+    model = Comentario
+
+class Detalles_comentarios(DetailView):
+    model = Comentario
+
+class Lista_valoraciones(ListView):
+    model = Valoracion
+
+class Detalles_valoraciones(DetailView):
+    model = Valoracion
+
+class Lista_Articulos(ListView):
+    model = Articulo
+
+class Detalles_articulos(DetailView):
+    model = Articulo
+
+class Lista_usuarios(ListView):
+    model = Usuario
+
+class Detalles_usuarios(DetailView):
+    model = Usuario
 # Create your views here.
