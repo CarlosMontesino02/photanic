e31900f2c5bf08c9045e0fbcf28f481bb6797f8a
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-10-14 16:17:46 +0200

    Update admin.py

--- photanic/photanic_app/admin.py
+++ photanic/photanic_app/admin.py
@@ -6,4 +6,6 @@ admin.site.register(models.Usuario)
 admin.site.register(models.Planta)
 admin.site.register(models.Foto)
 admin.site.register(models.Comentario)
+admin.site.register(models.Articulo)
+admin.site.register(models.Valoracion)
 # Register your models here.
