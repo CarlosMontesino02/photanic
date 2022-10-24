11985fdc209b27a9dca49b8bdd20d35be06669c2
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-10-24 20:55:09 +0200

    Update urls.py

--- photanic/photanic/urls.py
+++ photanic/photanic/urls.py
@@ -15,25 +15,48 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from photanic_app.views import index, Lista_plantas, Lista_fotos, Lista_comentarios, Lista_valoraciones, Lista_Articulos, Lista_usuarios
-from photanic_app.views import plantcreateview, plantUpdateView, fotocreateview, fotoUpdateView, comentcreateview, comentUpdateView
-from photanic_app.views import Detalles_plantas, Detalles_fotos, Detalles_comentarios, Detalles_valoraciones, Detalles_articulos, Detalles_usuarios
+from photanic_app.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('plants/', Lista_plantas.as_view(), name="plants"),
     path('', index, name='index'),
-    path('photos/',Lista_fotos.as_view(), name="fotos"),
-    path('articles/', Lista_Articulos.as_view(), name="articles"),
-    path('profiles/', Lista_usuarios.as_view(), name="profiles"),
+    
+    #plantas
+    path('plants/', Lista_plantas.as_view(), name="plants"),
     path('plants/<int:pk>/', Detalles_plantas.as_view(), name='plants_details'),
-    path('articles/<int:pk>/', Detalles_articulos.as_view(), name="articles_details"),
-    path('plant/add', plantcreateview.as_view(), name="plant-add"),
+    path('plant/add/', plantcreateview.as_view(), name="plant-add"),
     path('plant/<int:pk>/update', plantUpdateView.as_view(), name='plant-update'),
-    path('foto/add', fotocreateview.as_view(), name="foto-add"),
-    path('foto/<int:pk>/update', fotoUpdateView.as_view(), name='foto-update'),
-    path('coment/add', comentcreateview.as_view(), name="coment-add"),
-    path('coment/<int:pk>/update', comentUpdateView.as_view(), name='coment-update'),
+    path('plants/<int:pk>/delete/', plantDeleteView.as_view(), name='plant-delete'),
+   
+    #Fotos
+    path('foto/',Lista_fotos.as_view(), name="fotos"),
+    path('foto/<int:pk>/', Detalles_fotos.as_view(), name='foto_details'),
+    path('foto/add/', fotocreateview.as_view(), name="foto-add"),
+    path('foto/<int:pk>/update/', fotoUpdateView.as_view(), name='foto-update'),
+    path('foto/<int:pk>/delete/', fotoDeleteView.as_view(), name='foto-delete'),
+   
+    #Articulos
+    path('articles/', Lista_Articulos.as_view(), name="articles"),
+    path('articles/<int:pk>/', Detalles_articulos.as_view(), name="articles_details"),
+    path('articles/add/', articlecreateview.as_view(), name="article-add"),
+    path('articles/<int:pk>/update/', articleUpdateView.as_view(), name='article-update'),
+    path('articles/<int:pk>/delete/', articleDeleteView.as_view(), name='article-delete'),
+  
+    #Comentarios
+    path('coments', Lista_comentarios.as_view(), name="coments"),
+    path('coments/<int:pk>/', Detalles_comentarios.as_view(), name="coments_details"),
+    path('coment/add/', comentcreateview.as_view(), name="coment-add"),
+    path('coment/<int:pk>/update/', comentUpdateView.as_view(), name='coment-update'),
+    path('coment/<int:pk>/delete/', comentDeleteView.as_view(), name='coment-delete'),
+
+    #Valoraciones
+    path('rate', Lista_comentarios.as_view(), name="rates"),
+    path('rate/<int:pk>/', Detalles_valoraciones.as_view(), name="rates_details"),
+    path('rate/add/', ratecreateview.as_view(), name="rate-add"),
+    path('rate/<int:pk>/update/', rateUpdateView.as_view(), name='rate-update'),
+    path('rate/<int:pk>/delete/', rateDeleteView.as_view(), name='rate-delete'),
+
+    path('profiles/', Lista_usuarios.as_view(), name="profiles"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
