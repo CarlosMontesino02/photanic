5c3b78c8cec757de78cad96ae9b53a53b4b5defd
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-01-09 19:53:38 +0100

    Add files via upload

--- photanic/photanic_app/api.py
+++ photanic/photanic_app/api.py
@@ -2,14 +2,21 @@ from django.urls import path, include
 from .models import *
 from rest_framework import routers, serializers, viewsets
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff','country','birth_date','rank']
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 # ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -18,6 +25,8 @@ class PlantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Planta
         fields = ['common_name','kingdom','phylum','clase','order','family','genus','category']
+        authentication_classes = [SessionAuthentication, BasicAuthentication]
+        permission_classes = [IsAuthenticated]
 
 class PlantViewSet(viewsets.ModelViewSet):
     queryset = Planta.objects.all()
@@ -33,14 +42,21 @@ class FotoViewSet(viewsets.ModelViewSet):
     queryset = Foto.objects.all()
     serializer_class = FotoSerializer
 
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
 class ArticuloSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Articulo
         fields = ['title', 'text', 'Usu_art', 'plant_art']
+        authentication_classes = [SessionAuthentication, BasicAuthentication]
+        permission_classes = [IsAuthenticated]
 
 class ArticuloViewSet(viewsets.ModelViewSet):
     queryset = Articulo.objects.all()
     serializer_class = ArticuloSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
