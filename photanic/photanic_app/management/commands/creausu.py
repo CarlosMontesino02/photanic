f010b0ae4d3526dbc9d70973c091bab7fd58c3c7
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-01-12 20:28:58 +0100

    Add files via upload

--- photanic/photanic_app/management/commands/creausu.py
+++ photanic/photanic_app/management/commands/creausu.py
@@ -6,8 +6,16 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('total', type=int)
-
+    
     def handle(self, *args, **kwargs):
+        usernames = ['abusivesimplistic','aircraft quadriceps',
+'anyway-guillemot','booger$scruined','earnest-bollard','eyeglasses*abundant',
+'gravelly.unripe','sandalunderstood!','highjumptogether','suffix_pallograph',
+'prettying^dismiss','limitation_dromond','miserable_wussle','purring.rostary',
+'scaffoldinglukewarm','sledding congress','turtlegrapping','walloverpottery',
+'wrestlers_scrawny']
+        cont=1
         total = kwargs['total']
         for i in range(total):
-            User.objects.create_user(username=get_random_string(5), email='ben@ben.com', password='Contraseña1234')
+            User.objects.create_user(username=usernames[cont], email='ben@ben.com', password='Contraseña1234')
+            cont+=1
