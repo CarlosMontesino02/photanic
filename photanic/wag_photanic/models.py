0950f6cb81fbd8c9fc6bf9c9d9f56d53b5139f46
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-01-23 21:26:50 +0100

    Add files via upload

---
+++ photanic/wag_photanic/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from photanic_app import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
+
+# Create your models here.
