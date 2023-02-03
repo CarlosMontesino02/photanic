9cc94af646fa4892abefc8b9c9239a3a02470259
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-02-03 16:18:39 +0100

    Add files via upload

--- photanic/wag_photanic/models.py
+++ photanic/wag_photanic/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from photanic_app import models
+#from photanic_app import *
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
