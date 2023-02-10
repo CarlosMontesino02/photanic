b9fd0dc74989ac207de4ad4cb4175e14262b6368
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-02-10 10:50:53 +0100

    Update blogger.py

--- photanic/photanic_app/management/commands/blogger.py
+++ photanic/photanic_app/management/commands/blogger.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import Group
-from gameseek_app.models import User, Client
+from photanic_app.models import User
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 from django.contrib.auth.signals import user_logged_in
@@ -13,7 +13,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         try:
             user_name = kwargs['usuario']
-            usuario = Client.objects.get(username=user_name)
+            usuario = User.objects.get(username=user_name)
             grupo = Group.objects.get(name='Blogger')
             if not grupo in usuario.groups.all():
                 usuario.groups.add(grupo)
