3c0180b0b1e00eed4d5cce08f685e1668b3b1f8b
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-02-10 17:23:45 +0100

    Add files via upload

--- photanic/wag_photanic/signals.py
+++ photanic/wag_photanic/signals.py
@@ -2,14 +2,21 @@ from django.contrib.auth.models import Group
 from photanic_app.models import User
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
+from django.core.mail import send_mail
+
 
 @receiver(user_logged_in)
 def log_user_login(sender, user, **kwargs):
     try:
         group = Group.objects.get(name='Blogger')
         usu = User.objects.get(username=user)
+        c = User.objects.get(username=user).mail
         if not group in usu.groups.all():
                 usu.groups.add(group)
                 usu.save()
+                subject = 'Añadido al grupo Bloggers'
+                mensaje = 'Este mail te informa de que tienes acceso a las caracterśiitcsa del grupo Bloggers'
+                from_email = 'qhgx.ghfaa89@nefyp.com'
+                send_mail(subject,mensaje,from_email, [f'{c}'])
     except:
         print(f"Hubo un error durante la agregación del usuario")
