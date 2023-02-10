01211d9fb7f1434b8aee3409d9042012afc4a29e
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-02-10 12:04:29 +0100

    Update settings.py

--- photanic/photanic/settings.py
+++ photanic/photanic/settings.py
@@ -35,47 +35,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 ##########################################################################
 #ldap
 # Baseline configuration.
-AUTH_LDAP_SERVER_URI = "ldap://127.0.0.1:1389"
+AUTH_LDAP_SERVER_URI = "ldap://localhost:1389"
+
+AUTH_LDAP_BIND_DN = "cn=admin,dc=example,dc=org"
+AUTH_LDAP_BIND_PASSWORD = "adminpassword"
 AUTH_LDAP_USER_SEARCH = LDAPSearch(
-    "ou=users,dc=example,dc=org", ldap.SCOPE_SUBTREE, "(cn=%(user)s)"
+    "ou=users,dc=example,dc=org", ldap.SCOPE_SUBTREE, "(uid=%(user)s)"
 )
-# Or:
-# AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,ou=users,dc=example,dc=com'
-
-# Set up the basic group parameters.
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
-    "ou=users,dc=example,dc=org",
-    ldap.SCOPE_SUBTREE,
-    "(objectClass=groupOfNames)",
-)
-AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
-
-# Simple group restrictions
-AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=django,ou=groups,dc=example,dc=com"
-AUTH_LDAP_DENY_GROUP = "cn=disabled,ou=django,ou=groups,dc=example,dc=com"
-
 # Populate the Django user from the LDAP directory.
 AUTH_LDAP_USER_ATTR_MAP = {
-    "first_name": "givenName",
-    "last_name": "sn",
     "email": "mail",
 }
-
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    "is_active": "cn=active,ou=django,ou=groups,dc=example,dc=com",
-    "is_staff": "cn=staff,ou=django,ou=groups,dc=example,dc=com",
-    "is_superuser": "cn=superuser,ou=django,ou=groups,dc=example,dc=com",
-}
-
-# This is the default, but I like to be explicit.
-AUTH_LDAP_ALWAYS_UPDATE_USER = True
-
-# Use LDAP group membership to calculate group permissions.
-AUTH_LDAP_FIND_GROUP_PERMS = True
-
-# Cache distinguished names and group memberships for an hour to minimize
-# LDAP traffic.
-AUTH_LDAP_CACHE_TIMEOUT = 3600
 
 # Keep ModelBackend around for per-user permissions and maybe a local
 # superuser.
@@ -83,6 +53,10 @@ AUTHENTICATION_BACKENDS = (
     "django_auth_ldap.backend.LDAPBackend",
     "django.contrib.auth.backends.ModelBackend",
 )
+
+WAGTAILUSERS_PASSWORD_ENABLED = False
+WAGTAIL_PASSWORD_MANAGEMENT_ENABLED = False
+WAGTAIL_PASSWORD_RESET_ENABLED = False
 ##########################################################################3
 # Application definition
 
