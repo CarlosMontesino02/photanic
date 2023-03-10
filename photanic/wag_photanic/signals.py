34381ab9a92fdc121f009ff95fe2450d3fb130af
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-03-10 11:50:28 +0100

    Add files via upload

--- photanic/wag_photanic/signals.py
+++ photanic/wag_photanic/signals.py
@@ -1,25 +1,30 @@
 from django.contrib.auth.models import Group
 from photanic_app.models import User
-from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.signals import (user_logged_in,
+                                        user_logged_out,
+                                        user_login_failed)
 from django.dispatch import receiver
-from django.core.mail import send_mail
-
+from django.core.mail import BadHeaderError,send_mail
 
 @receiver(user_logged_in)
 def log_user_login(sender, user, **kwargs):
     try:
-        group = Group.objects.get(name='Blogger')
-        usu = User.objects.get(username=user)
+        grupo = Group.objects.get(name='Blogger')
+        usuario = User.objects.get(username=user)
         c = User.objects.get(username=user).email
-        if not group in usu.groups.all():
-                usu.groups.add(group)
-                usu.save()
-                send_mail(
-                'Hola!, te hemos asignado al grupo Bloggers',
-                'Apartir de ahora tienes la capacidad de escribir artículos en nuestra aplicación!',
-                'photanic.oficial@gmail.com',
-                [c],
-                fail_silently=False,
-            )
     except:
-        print(f"Hubo un error durante la agregación del usuario")
+         print(f"ERROR: No se ha podido asignar alguna/s de las variables.")
+
+    if not grupo in usuario.groups.all():
+        usuario.groups.add(grupo.id)
+        usuario.save()
+        print(f"El usuario, {usuario.username}, se ha añadido al grupo, {grupo.name}, correctamente.")
+        send_mail(
+            'Hola!, te hemos asignado al grupo Bloggers',
+            'Apartir de ahora tienes la capacidad de escribir artículos en nuestra aplicación!',
+            'photanic.oficial@gmail.com',
+            [c],
+            fail_silently=False,
+        )
+    else:
+        print(f"ERROR: El usuario, {usuario.username}, ya se encuentra al grupo {grupo.name}.")
