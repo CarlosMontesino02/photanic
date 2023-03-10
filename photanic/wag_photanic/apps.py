4bd95b4db7ffceb3e8db52b942ed3686f82e2591
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2023-03-10 11:55:47 +0100

    Update apps.py

--- photanic/wag_photanic/apps.py
+++ photanic/wag_photanic/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class WagPhotanicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wag_photanic'
-    def read(self):
+    def ready(self):
         import wag_photanic.signals
